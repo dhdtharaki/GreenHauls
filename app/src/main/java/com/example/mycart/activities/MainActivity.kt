91cd0da6b5890aee7a7a512613243149c51c9b60
IT21302626 <it21302626@my.sliit.lk>
2023-05-05 08:59:24 +0530

    updated

--- app/src/main/java/com/example/mycart/activities/MainActivity.kt
+++ app/src/main/java/com/example/mycart/activities/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.mycart.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mycart.R
 
 
@@ -11,6 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var btnInsertData: Button
     private lateinit var btnFetchData: Button
+
+    // declare a variable to keep track of the total number of items in the cart
+    private var totalItemsInCart = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,14 +26,21 @@ class MainActivity : AppCompatActivity() {
 
         //navigate to the add items page
         btnInsertData.setOnClickListener {
+
+            // increment the total number of items in the cart each time an item is added
+            totalItemsInCart++
+
             val intent = Intent(this, InsertionActivity::class.java)
             startActivity(intent)
+
         }
 
         //navigate to the my cart
         btnFetchData.setOnClickListener {
             val intent = Intent(this, FetchingActivity::class.java)
             startActivity(intent)
+            // display the total number of items in the cart as a message in the "View Your Cart" button
+            btnFetchData.text = "View Your Cart (${totalItemsInCart} items)"
         }
 
     }
