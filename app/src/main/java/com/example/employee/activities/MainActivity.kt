47e2d03dbc2f6af5c731727ff37586878609d64d
Senuri <it21293894@my.sliit.lk>
2023-05-05 20:56:12 +0530

    seller

--- app/src/main/java/com/example/employee/activities/MainActivity.kt
+++ app/src/main/java/com/example/employee/activities/MainActivity.kt
@@ -4,34 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.employee.R
+import com.google.firebase.database.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnInsertData: Button
     private lateinit var btnFetchData: Button
-
+    private lateinit var tvCount: TextView
+    private lateinit var dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         btnInsertData = findViewById(R.id.btnInsertData)
         btnFetchData = findViewById(R.id.btnFetchData)
+        tvCount = findViewById(R.id.tvCount)
+        dbRef = FirebaseDatabase.getInstance().getReference("Employees")
 
-        //add sellers button
+        //navigate to add sellers page
         btnInsertData.setOnClickListener {
             val intent = Intent(this, InsertionActivity::class.java)
             startActivity(intent)
         }
 
-        //all sellers button
+        //navigate to all sellers page
         btnFetchData.setOnClickListener {
             val intent = Intent(this, FetchingActivity::class.java)
             startActivity(intent)
         }
 
+        //count all sellers
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val count = snapshot.childrenCount.toInt()
+                tvCount.text = "Number of sellers: $count"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //handle error
+            }
+        })
     }
 }
